f4959db446352fcaf08a1282d87a0dd793a26866
Bessenyei Csilla <bessenyei.csilla@gmail.com>
2019-12-27 15:45:43 +0000

    Adding card to trello

--- wanttoread/wanttoread.py
+++ wanttoread/wanttoread.py
@@ -2,18 +2,54 @@
 
 import bs4
 import urllib.request
+import config
+from pprint import pprint
+from trello import TrelloApi
 
-reading_speed_per_min = 200
+
 url = "https://www.leanproduction.com/theory-of-constraints.html"
 
-from urllib.request import Request, urlopen
+def calculate_read_time(url):
 
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
+	from urllib.request import Request, urlopen
 
-soup = bs4.BeautifulSoup(webpage, features="html.parser")
-webpage_text = soup.get_text()
-word_count = len(webpage_text.split())
-time_to_read_in_min = word_count/reading_speed_per_min
+	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	webpage = urlopen(req).read()
 
-print(time_to_read_in_min)
+	soup = bs4.BeautifulSoup(webpage, features="html.parser")
+	title = soup.title.text
+	webpage_text = soup.get_text()
+	word_count = len(webpage_text.split())
+	time_to_read_in_min = word_count/config.READING_SPEED_PER_MIN
+	return title, time_to_read_in_min
+
+def setup():
+	trello = TrelloApi(config.TRELLO_APP_KEY)
+	trello.set_token(config.USER_TOKEN)
+	return trello
+
+def get_board_list(trello, url, time_to_read_in_min):
+	board_ids = trello.members.get("csillabessenyei")["idBoards"]
+	return board_ids
+
+def print_boards(trello, board_list):
+	print("ID, Board Name")
+	for b in board_list:
+		print(b, trello.boards.get(b)["name"])
+
+
+def get_lists_list(trello):
+	for list in trello.boards.get_list(config.BOARD_ID):
+		print(list["id"], list["name"])
+
+def create_card(trello):
+	name = f"[Read Article] ({time_to_read_in_min} minutes) {title}"
+	trello.cards.new(idList=config.LIST_ID, name=name, desc=url)
+
+title, time_to_read_in_min = calculate_read_time(url)
+
+trello = setup()
+board_list = get_board_list(trello, url, time_to_read_in_min)
+# print_boards(trello, board_list)
+# get_lists_list(trello)
+create_card(trello)
