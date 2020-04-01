92984cd25e63249a0db2b14916bb948333f42383
Bessenyei Csilla <bessenyei.csilla@gmail.com>
2020-04-01 21:49:49 +0100

    Config is now yaml

--- wanttoread/wanttoread/wanttoread.py
+++ wanttoread/wanttoread/wanttoread.py
@@ -2,16 +2,20 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
 import sys
+import yaml
 from trello import TrelloApi
 import bs4
 
-import wanttoread.config as config
 import requests
 
+def _get_config():
+    with open("wanttoread/config.yaml") as file_handler:
+        return yaml.load(file_handler, Loader=yaml.FullLoader)
 
 def authenticate():
-    trello = TrelloApi(config.TRELLO_APP_KEY)
-    trello.set_token(config.TRELLO_USER_TOKEN)
+    config = _get_config()
+    trello = TrelloApi(config["TRELLO_APP_KEY"])
+    trello.set_token(config["TRELLO_USER_TOKEN"])
     return trello
 
 
@@ -23,28 +27,29 @@ def parse_page(url):
 def get_title(page_soup):
     return page_soup.title.text
 
-def get_readtime_in_min(page_soup):
+def get_readtime_in_min(page_soup, reading_speed_per_min):
     tags_to_remove = ["script", "style"]
     for undesired_tag in page_soup.find_all(tags_to_remove):
         undesired_tag.decompose()
 
     webpage_text = page_soup.get_text()
     word_count = len(webpage_text.split())
-    return word_count / config.READING_SPEED_PER_MIN
+    return word_count / reading_speed_per_min
 
-def create_card(title, url, time_to_read_in_min, trello):
+def create_card(title, url, time_to_read_in_min, trello, trello_list_id):
     name = f"[Read Article] ({time_to_read_in_min} minutes) {title}"
-    trello.cards.new(idList=config.TRELLO_LIST_ID, name=name, desc=url)
+    trello.cards.new(idList=trello_list_id, name=name, desc=url)
     print(f"Card created: {name}")
 
 
 def wtr_page(url):
     trello = authenticate()
+    config = _get_config()
     page_soup = parse_page(url)
     title = get_title(page_soup)
-    readtime_in_min = get_readtime_in_min(page_soup)
+    readtime_in_min = get_readtime_in_min(page_soup, config["READING_SPEED_PER_MIN"])
 
-    create_card(title, url, readtime_in_min, trello)
+    create_card(title, url, readtime_in_min, trello, config["TRELLO_LIST_ID"])
 
 def wtr_get_lists_list(board_id):
     trello = authenticate()
